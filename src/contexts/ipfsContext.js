25e9163712cef3dd6de7bc6a3f39ab3dfe5a9c60
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-07-03 13:47:14 +0200

    removing webrtc

--- src/contexts/ipfsContext.js
+++ src/contexts/ipfsContext.js
@@ -10,16 +10,7 @@ export const IpfsContextProvider = ({ children }) => {
 
   useEffect(() => {
     async function fetchData() {
-      let ipfs = await create({
-        config: {
-          Addresses: {
-            Swarm: [
-              "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
-              "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
-            ],
-          },
-        },
-      });
+      let ipfs = await create();
       setIpfs(ipfs);
     }
     fetchData();
